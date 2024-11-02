eab8887adceec524f219ace1d5c92a75ac16c28a
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2024-11-02 13:00:43 -0300

    seguridad GPT

--- src/main/java/com/claujulian/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/claujulian/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("MI_CLAVE");
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
